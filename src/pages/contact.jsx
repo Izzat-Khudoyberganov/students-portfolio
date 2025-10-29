09a97820561a1731afe5703ba7147bba2f705a4a
izzat <izzatdesign001@gmail.com>
2025-10-29 16:06:04 +0500

    added contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,14 +1,54 @@
-
 const Contact = () => {
   return (
-    <section>
+    <section className="mt-40">
       <div className="container">
-        <h1 className="text-4xl font-bold mb-10">Contact Us</h1>
-        <p className="text-lg text-gray-700">
-          If you have any questions, suggestions, or would like to get in touch, please feel free to reach out to us at </p>
-          </div>
-    </section>
-  )
-}
+        <form className="flex flex-col gap-5 bg-gray-50 p-5 rounded-lg">
+          <label htmlFor="name" className="text-2xl font-normal">
+            Ism
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Ismingizni kiriting"
+            className="p-4 border-[1px] border-b-slate-500 rounded-lg"
+            required
+          />
 
-export default Contact
+          <label htmlFor="phone" className="text-2xl font-normal">
+            Telefon
+          </label>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            placeholder="+998 99 999 99 99"
+            className="p-4 border-[1px] border-b-slate-500 rounded-lg"
+            required
+          />
+
+          <label htmlFor="message" className="text-2xl font-normal">
+            Xabar
+          </label>
+          <textarea
+            placeholder="Xabaringizni kiriting"
+            className="p-4 border-[1px] border-b-slate-500 rounded-lg resize-none "
+            required
+            id="message"
+            rows={5}
+            minLength={10}
+          ></textarea>
+
+          <button
+            type="submit"
+            className="bg-slate-900 text-white py-4 rounded-lg hover:bg-slate-700 transition"
+          >
+            Yuborish
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
